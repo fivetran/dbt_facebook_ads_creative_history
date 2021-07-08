8d2daf98074561a8e24492969928e76271561312
Dylan Baker <dylan@dbanalytics.co>
2021-07-07 22:15:51 -0400

    initial draft of postgres models

---
+++ models/postgres/int__facebook_ads__carousel_media_prep.sql
@@ -0,0 +1,36 @@
+with base as (
+
+    select *
+    from {{ ref('stg_facebook_ads__creative_history') }}
+
+), required_fields as (
+
+    select
+        _fivetran_id,
+        creative_id,
+        object_story_link_data_child_attachments,
+        object_story_link_data_caption,
+        object_story_link_data_description,
+        object_story_link_data_link,
+        object_story_link_data_message
+    from base
+    where object_story_link_data_child_attachments is not null
+
+), unnested as (
+
+    select
+        _fivetran_id,
+        creative_id,
+        object_story_link_data_caption as caption,
+        object_story_link_data_description as description,
+        object_story_link_data_message as message,
+        element->>'link' as link,
+        element->'url_tags' as url_tags,
+        row_number() over (partition by _fivetran_id, creative_id) as index
+    from required_fields
+    left join lateral json_array_elements(object_story_link_data_child_attachments::json) as element on True
+
+)
+
+select *
+from unnested
---
+++ models/postgres/stg_facebook_ads__carousel_media_url_tags.sql
@@ -0,0 +1,20 @@
+with base as (
+
+    select *
+    from {{ ref('int__facebook_ads__carousel_media_prep') }}
+
+), unnested as (
+
+    select
+        _fivetran_id,
+        creative_id,
+        index,
+        element->>'key' as key,
+        element->>'value' as value
+    from base
+    inner join lateral json_array_elements(url_tags) as element on True
+
+)
+
+select *
+from unnested
