bf9a68995530499019e0e88843904eaed8f23330
alxd <github@alxd.org>
2021-11-16 15:17:41 +0100

    add source_relation to bigquery

--- models/bigquery/stg_facebook_ads__url_tag.sql
+++ models/bigquery/stg_facebook_ads__url_tag.sql
@@ -8,7 +8,8 @@ with base as (
     select
         _fivetran_id,
         creative_id,
-        url_tags
+        url_tags,
+        source_relation
     from base
     where url_tags is not null
 
@@ -17,14 +18,15 @@ with base as (
     select
         _fivetran_id,
         creative_id,
-        json_extract_array(replace(trim(url_tags, '"'),'\\','')) as cleaned_url_tags
+        json_extract_array(replace(trim(url_tags, '"'),'\\','')) as cleaned_url_tags,
+        source_relation
     from required_fields
 
 ), unnested as (
 
-    select _fivetran_id, creative_id, url_tag_element
+    select _fivetran_id, creative_id, url_tag_element, source_relation
     from cleaned_json
-    left join unnest(cleaned_url_tags) as url_tag_element
+    left join unnest(cleaned_url_tags) and source_relation as url_tag_element
     where cleaned_url_tags is not null
 
 ), fields as (
@@ -34,7 +36,8 @@ with base as (
         creative_id,
         json_extract_scalar(url_tag_element, '$.key') as key,
         json_extract_scalar(url_tag_element, '$.value') as value,
-        json_extract_scalar(url_tag_element, '$.type') as type
+        json_extract_scalar(url_tag_element, '$.type') as type,
+        source_relation
     from unnested
 
 )
